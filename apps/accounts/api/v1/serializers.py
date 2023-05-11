1807deb174b6f11a74b5b5f3145e7f87521d358a
Murodali Narzullaev <murodalinarzullaevofficial@gmail.com>
2023-05-11 18:08:51 +0500

    modified

--- apps/accounts/api/v1/serializers.py
+++ apps/accounts/api/v1/serializers.py
@@ -10,22 +10,34 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from ...models import Account
 
 
-class RegisterSerializer(serializers.ModelSerializer):
+class VerifyNumberSerializer(serializers.Serializer):
+    phone = serializers.CharField(min_length=12)
     verification_code = serializers.CharField(min_length=4, max_length=4, write_only=True)
-    class Meta:
-        model = Account
-        fields = ('phone', 'verification_code')
 
     def validate(self, attrs):
+        phone = attrs.get('phone')
+        attrs['registered'] = False
         verification_code = attrs.get('verification_code', None)
         if verification_code != os.environ.get('VERIFICATION_CODE'):
             raise ValidationError({"verification_code": _("Verification code did not match")})
+        user = authenticate(username=phone, password=os.environ.get('PASS'))
+        if user:
+            attrs['registered'] = True
         return attrs
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = ('phone', 'first_name', 'last_name', 'birth_date', 'avatar')
+        extra_fields = {
+            'avatar': {'required': False}
+        }
 
     def create(self, validated_data):
         password = os.environ.get('PASS')
-        del validated_data['verification_code']
         validated_data['password'] = password
+        validated_data['is_verified'] = True
         return Account.objects.create_user(**validated_data)
 
 
@@ -49,3 +61,12 @@ class LoginSerializer(serializers.Serializer):
         if not user.is_verified:
             raise AuthenticationFailed({'detail': 'Account is not verified'})
         return user
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = ['id', 'phone', 'first_name', 'last_name', 'avatar', 'is_superuser', 'is_staff', 'is_active',
+                  'is_verified', 'created_date', 'modified_date']
+        read_only_fields = ['is_superuser', 'is_staff', 'is_active', 'is_verified', 'created_date', 'modified_date']
+
--- apps/accounts/api/v1/urls.py
+++ apps/accounts/api/v1/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import AccountRegisterView, AccountLoginView
+from .views import VerifyNumberView, AccountRegisterView, AccountLoginView, AccountListView, AccountRUDView
 
 urlpatterns = [
+    path('verify_number/', VerifyNumberView.as_view()),
     path('register/', AccountRegisterView.as_view()),
     path('login/', AccountLoginView.as_view()),
+
+    path('account/list/', AccountListView.as_view()),
+    path('account/rud/<int:pk>/', AccountRUDView.as_view()),
 
 ]
