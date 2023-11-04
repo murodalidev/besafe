d868a8e62047426a94dcd9984139d10d37e626d1
murodalidev <murodalinarzullaevofficial@gmail.com>
2023-11-04 17:25:16 +0500

    modified

--- apps/alerts/api/v1/urls.py
+++ apps/alerts/api/v1/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SendAlertTelegram
+from .views import SendAlertTelegram, SendAnonymousTelegram
 
 
 urlpatterns = [
-    path('send-telegram/', SendAlertTelegram.as_view())
+    path('send-telegram/', SendAlertTelegram.as_view()),
+    path('send-telegram-anonymous/', SendAnonymousTelegram.as_view()),
 ]
